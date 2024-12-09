4be932e795fea56139096c64a8110ba9e18c9bf9
Cliente CONTROL-PC <CltControl@GGIECLUGWRK163.espol.edu.ec>
2024-12-09 10:54:46 -0500

    second

---
+++ src/credentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBxMi7lEZppqikdNVOkjvJqRgWsYDLUONg",
+    authDomain: "hibrida-34f05.firebaseapp.com",
+    databaseURL: "https://hibrida-34f05-default-rtdb.firebaseio.com",
+    projectId: "hibrida-34f05",
+    storageBucket: "hibrida-34f05.firebasestorage.app",
+    messagingSenderId: "1044174519888",
+    appId: "1:1044174519888:web:adb02f9b5e6c85f19f9b55"
+};
