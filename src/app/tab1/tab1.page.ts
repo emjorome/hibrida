189a3e5aaf6767629993025228963e506d299522
emjorome <emjorome@espol.edu.ec>
2024-12-11 10:13:21 -0500

    thrid

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, signal } from '@angular/core';
+import { IonFab, IonFabButton, IonIcon, IonCard, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+/* Importe la función y el ícono */
+import { addIcons } from 'ionicons';
+import { cloudUploadOutline } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonFab, IonFabButton, IonIcon, IonCard, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab1Page {
-  constructor() {}
+  imageReady = signal(false)
+  imageUrl = signal("")
+
+  constructor() {
+    /* Registre el ícono */
+    addIcons({ cloudUploadOutline });
+  }
+
+  /* El método onSubmit para enviar los datos del formulario mediante el servicio */
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const reader = new FileReader();
+
+      // Convertir el archivo a una URL base64 para mostrarlo en el html
+      reader.onload = () => {
+        console.log(reader.result as string)
+        this.imageUrl.set(reader.result as string)
+        this.imageReady.set(true)
+      };
+
+      reader.readAsDataURL(file); // Leer el archivo como base64
+    }
+  }
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import {  clipboardOutline, triangle, ellipse, square, peopleCircle } from 'ionicons/icons';
+import {  rocket, clipboardOutline, triangle, ellipse, square, peopleCircle } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -14,6 +14,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ clipboardOutline, triangle, ellipse, square, peopleCircle });
+    addIcons({ rocket, clipboardOutline, triangle, ellipse, square, peopleCircle });
   }
 }
